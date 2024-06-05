b37e6e67e49f5833e28e74725db422626c8620d2
Songhygoingpro <123635791+Songhygoingpro@users.noreply.github.com>
2024-06-05 12:49:34 +0700

    again

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 /*block reveal animation*/
-const hero_section = document.querySelectorAll('.hero-section .reveal-text span');
+const hero_section = document.querySelectorAll('.hero-section .reveal-text [data-aos="custom-fade-up"]');
 hero_section.forEach((element, index) => {
     element.setAttribute('data-aos-delay', `${index * 100}`);
 });
@@ -9,7 +9,7 @@ fade_up.forEach((element, index) => {
     element.setAttribute('data-aos-delay', `${index * 100}`);
 });
 
-const information_section = document.querySelectorAll('.information-section .reveal-text span');
+const information_section = document.querySelectorAll('.information-section .reveal-text [data-aos="custom-fade-up"]');
 information_section.forEach((element, index) => {
     element.setAttribute('data-aos-delay', `${index * 100}`);
 });
