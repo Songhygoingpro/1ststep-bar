d65e21a58239a66c4483e078109e5aac9a68eeaa
Songhygoingpro <123635791+Songhygoingpro@users.noreply.github.com>
2024-06-05 10:29:43 +0700

    done menu page

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
-/*headline animation*/
-document.addEventListener('DOMContentLoaded', () => {
-    const headlines = document.querySelectorAll('.headline span');
-    headlines.forEach((span, index) => {
-        span.style.animationDelay = `${index * 0.2}s`;
-    });
+/*block reveal animation*/
+const hero_section = document.querySelectorAll('.hero-section__inner .reveal-text span');
+hero_section.forEach((element, index) => {
+    element.setAttribute('data-aos-delay', `${index * 200}`);
 });
+
+const elements = document.querySelectorAll('[data-aos="fade-up"]');
+        elements.forEach((element, index) => {
+            element.setAttribute('data-aos-delay', `${index * 200}`);
+        });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
     extend: {},
   },
