9c0d5e61c6fcfcd20344dbe4ed3283df3ef5fb31
Songhygoingpro <123635791+Songhygoingpro@users.noreply.github.com>
2024-06-07 22:58:05 +0700

    again

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*block reveal animation*/
-const hero_section = document.querySelectorAll('.hero-section .reveal-text [data-aos="custom-fade-up"]');
+const hero_section = document.querySelectorAll('.hero-section .reveal-block [data-aos="custom-fade-up"]');
 hero_section.forEach((element, index) => {
     element.setAttribute('data-aos-delay', `${index * 100}`);
 });
@@ -9,12 +9,12 @@ fade_up.forEach((element, index) => {
     element.setAttribute('data-aos-delay', `${index * 100}`);
 });
 
-const information_section = document.querySelectorAll('.information-section .reveal-text [data-aos="custom-fade-up"]');
+const information_section = document.querySelectorAll('.information-section .reveal-block [data-aos="custom-fade-up"]');
 information_section.forEach((element, index) => {
     element.setAttribute('data-aos-delay', `${index * 100}`);
 });
 
-const recruit_section = document.querySelectorAll('.recruit-section .reveal-text [data-aos="custom-fade-up"]');
+const recruit_section = document.querySelectorAll('.recruit-section .reveal-block [data-aos="custom-fade-up"]');
 recruit_section.forEach((element, index) => {
     element.setAttribute('data-aos-delay', `${index * 100}`);
 });
